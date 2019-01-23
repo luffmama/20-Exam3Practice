df05161b3c60c5b35e2ccb1abbcfbe2b3d72eb1c
luffmama <luffmama@rose-hulman.edu>
2019-01-23 10:13:49 -0500

    DONE

--- src/m6_loops_within_loops_printing.py
+++ src/m6_loops_within_loops_printing.py
@@ -6,8 +6,8 @@ This problem provides practice at:
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Margaret Luffman.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE
 
 ###############################################################################
 # Students:
@@ -76,7 +76,7 @@ def shape(r):
     For purposes of "lining up", assume r is a single digit.
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     ###########################################################################
@@ -88,7 +88,18 @@ def shape(r):
     #    DIFFICULTY:      7
     #    TIME ESTIMATE:  15 minutes.
     # -------------------------------------------------------------------------
-
+    for k in range(r):
+        for a in range(k):
+            print(" ",end="")
+        for b in range(r-k):
+            print("+",end="")
+        for c in range(1):
+            print("!",end="")
+        for d in range(r-k,0,-1):
+            print(d,end="")
+        for e in range(k):
+            print("-",end="")
+        print()
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
